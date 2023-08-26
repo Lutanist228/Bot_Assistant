29e460b587a5495845c2658a982e2bfd81ed65d3
Lutanist <alexzavr003@gmail.com>
2023-08-27 00:21:03 +0300

    Commit v-on 3

--- message_handlers.py
+++ message_handlers.py
@@ -1,4 +1,5 @@
 from main import dp, my_bot
+import asyncio as asy
 from aiogram import types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
@@ -6,6 +7,10 @@ from sql import db_start, add_message, extract_message
 from additional_functions import file_reader, save_to_txt
 from buttons import get_cancel, get_start
 count = 0
+
+def counter_func(obj: int):
+    obj += 1
+    yield obj
 
 class Question_Processing(StatesGroup):
     question = State()
@@ -30,30 +35,26 @@ async def start_message(message: types.Message, state: FSMContext):
 async def recieving_message(message: types.Message, state: FSMContext):
     await state.update_data(question=message.text)
     data = await state.get_data()
-    await add_message(data)
+    await add_message(message_text=data, user_id=message.from_user.id)
     await message.answer(text="Отлично, а теперь дождитесь ответа бота!", reply_markup=get_cancel())
     await state.set_state("reply")
-
-@dp.message_handler(state=Question_Processing.reply)
-async def sending_reply(message: types.Message, state: FSMContext):
     message_text = await extract_message() 
     # тут будет обработка через отправку сообщения GPT. Затем ответ возвращается,
     # сохраняется и передается в переменную answer.
+    await asy.sleep(60), my_bot.send_message(chat_id=message.from_user.id, text=f"Ответ:\n{message_text}")
 
-    if message.text == "Отменить вопрос":
+@dp.message_handler(lambda message: message) 
+async def on_reply_processing(message: types.Message, state: FSMContext):
+    if message.text == "Завершить процесс":
         await message.reply("Действие отменено.\nВозврат в меню бота...", reply_markup=get_start())
         await state.finish()
     else:
         global count
-        count += 1
+        next(counter_func(count))
         await message.reply(text="""В настоящий момент ваш вопрос обрабатывается,\n просим проявить терпение).""")
-        yield count
         await message.delete()
     # до тех пор, пока пользователь не получил ответ, любое его сообщение будет игнорироваться 
     # необходимо поставить антифлуд на данный хендлер через MiddleWare
-    await my_bot.send_message(chat_id=message.from_user.id, text=f"Ответ:\n{message_text}")
-    await state.finish()
-
 
 
 
