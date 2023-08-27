267d42541b14f2bf1165d48e39957993051d622a
Lutanist <alexzavr003@gmail.com>
2023-08-27 11:27:12 +0300

    SQL commit v-on 1

--- sql.py
+++ sql.py
@@ -6,9 +6,9 @@ async def db_start():
     cur.execute("""CREATE TABLE IF NOT EXISTS user_data (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 user_id INTEGER NOT NULL,
-                quarry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                quarry_date TEXT, 
                 question_text TEXT NULL,
-                question_status ENUM("not sent", "sent", "under processing") NOT NULL DEFAULT "not sent",
+                question_status TEXT CHECK(question_status IN ('not sent','sent','under processing')) NOT NULL DEFAULT "not sent",
                 reply_text TEXT NULL
                 )""")
     db.commit()
@@ -17,11 +17,11 @@ async def add_message(message_text, user_id):
     db = sq.connect("message_db")
     cur = db.cursor()
     message_text = message_text.get("question")
-    cur.execute("INSERT INTO user_data (question_text, user_id) VALUES(?, ?)", (message_text, user_id))
+    cur.execute("INSERT INTO user_data (question_text, user_id, quarry_date) VALUES(?, ?, datetime('now', '+3 hours'))", (message_text, user_id))
     db.commit()
 
 async def extract_message():
     db = sq.connect("message_db")
     cur = db.cursor()
-    return cur.execute("SELECT message_text FROM user_data WHERE id = 1").fetchone()
+    return cur.execute("SELECT question_text FROM user_data WHERE id = 1").fetchone()
 
