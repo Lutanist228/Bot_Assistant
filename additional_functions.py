c29992b0b26064048f5f1e1caf9b31f301c14500
Lutanist <alexzavr003@gmail.com>
2023-08-29 21:13:26 +0300

    Version №1 for remote git

--- additional_functions.py
+++ additional_functions.py
@@ -1,12 +1,22 @@
-def file_reader(path = "GPT_pattern.txt"):
-    with open(file=rf"{path}", mode='r', buffering=-1, encoding="utf-8") as file:
-        pattern_text = file.readlines() 
+def boltun_file_reader():
+    with open(file="boltun.txt", mode='r', buffering=-1, encoding="utf-8") as file:
+        pattern_text = file.readlines() ; pattern_text = list(map(lambda x: x.lower().strip(), pattern_text))
         return pattern_text
         
-def save_to_txt(file_path: str, print_as_finished = True, **kwargs):
+def save_to_txt(file_path: str = "", print_as_finished = True, save_mode: str = "a", **kwargs):
+        
+        r"""Функция save_to_txt принимает в себя:
+        1) file_path - путь к файлу в формате: C:\Users\user\*file_dir*\. в случае, если нет необходимости 
+        сохранять файл в конкретную директорию, то файл сохраняется в директорию скрипта с save_to_txt;
+        2) print_as_finished - флаг, который контролирует вывод надписи The information has been added to the {file_name}.txt file.;
+        3) save_mode - формат работы с .txt файлом, по умолчанию - 'a';
+        4) **kwargs - основа функции, где key - назавние файла, а value - содержимое файла;
+        
+
+        """
         for key, value in kwargs.items():
             file_name = key
-            with open(f"{file_path}\\{file_name}.txt", mode="w+", buffering=-1, encoding="utf-8") as file:
+            with open(rf"{file_path}{file_name}.txt", mode=save_mode, buffering=-1, encoding="utf-8") as file:
                 if isinstance(value, (tuple, list)):
                     [file.write(val) for val in value]
                 else:
