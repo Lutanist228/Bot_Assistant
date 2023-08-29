f1606c66096218b290feb17efc739c6abb7af333
Lutanist <alexzavr003@gmail.com>
2023-08-30 00:35:58 +0300

    Git V-on №2

--- buttons.py
+++ buttons.py
@@ -4,4 +4,4 @@ def get_start() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Start"))
 
 def get_cancel() -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Отменить вопрос"))
+    return ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Завершить процесс"))
