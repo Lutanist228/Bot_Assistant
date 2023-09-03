fe9e3c4c2efe8c3e31af7f22ba47d14019550e06
Egor Derevyanko <derevyanko.egor2705@gmail.com>
2023-09-03 15:16:49 +0300

    edit 1

--- Chat_gpt_module.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-# from config import OPEN_AI_API
-
-async def start_data():
-    async with open('boltun.txt', encoding='utf8') as f:
-        lines = f.read()
-        print(lines)
-        
