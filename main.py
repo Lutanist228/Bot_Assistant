bd1640486b6b846873fc1db3f8cbc4a2d4026adf
Lutanist <alexzavr003@gmail.com>
2023-09-16 22:13:19 +0300

    Неважные фиксы

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def on_shutdown(_):
 
 if __name__ == '__main__':
     from user_message_handlers import dp
-    from moder_message_handlers import db
-    from user_callback_handlers import db
-    from moder_callback_handlers import db
+    from user_callback_handlers import dp
+    from moder_message_handlers import dp
+    from moder_callback_handlers import dp
     
     executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=lambda dp: on_startup_wrapper(dp), on_shutdown=on_shutdown)
